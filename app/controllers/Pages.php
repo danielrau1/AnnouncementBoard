8558d6114975882b045046d9e6340f35e9873ac3
Daniel <rauddaniel@yahoo.com>
2019-04-09 15:41:30 -0400

    first commit

---
+++ app/controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pages extends Controller{
+
+
+    public function __construct(){
+
+
+    }
+
+
+    public function index(){
+
+        $data = [
+            'title'=>'AnnouncementBoard'
+
+        ];
+
+        $this->view('pages/index',$data);
+    }
+
+
+
+}
---
+++ app/views/pages/index.php
@@ -0,0 +1,11 @@
+<h1><?php echo $data['title']; ?></h1>
+<?php echo APPROOT; ?>
+
+<h1>This is pages/index</h1>
+
+
+<ul>
+<li>
+    <a href="<?php echo URLROOT;?>/users/teachersLogin" >Teachers Login</a>
+</li>
+</ul>
